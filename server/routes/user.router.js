f8aae75fa8ddd87473b978460222f89e9813b5bf
Benji Copan <benji1220@gmail.com>
2021-08-04 13:01:54 -0500

    edited user.router and RegisterForm to allow for extra fields in user table

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -20,11 +20,15 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 router.post('/register', (req, res, next) => {
   const username = req.body.username;
   const password = encryptLib.encryptPassword(req.body.password);
+  const email = req.body.email;
+  const first_name = req.body.first_name;
+  const last_name = req.body.last_name;
+  const image_url = req.body.image_url;
 
-  const queryText = `INSERT INTO "user" (username, password)
-    VALUES ($1, $2) RETURNING id`;
+  const queryText = `INSERT INTO "user" (username, password, email, first_name, last_name, image_url)
+    VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`;
   pool
-    .query(queryText, [username, password])
+    .query(queryText, [username, password, email, first_name, last_name, image_url])
     .then(() => res.sendStatus(201))
     .catch((err) => {
       console.log('User registration failed: ', err);
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import './RegisterForm.css';
 
 function RegisterForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [first_name, setFirstName] = useState('');
+  const [last_name, setLastName] = useState('');
+  const [image_url, setImageUrl] = useState('');
+
   const errors = useSelector((store) => store.errors);
   const dispatch = useDispatch();
 
@@ -15,6 +21,10 @@ function RegisterForm() {
       payload: {
         username: username,
         password: password,
+        email: email,
+        first_name: first_name,
+        last_name: last_name,
+        image_url: image_url,
       },
     });
   }; // end registerUser
@@ -27,7 +37,7 @@ function RegisterForm() {
           {errors.registrationMessage}
         </h3>
       )}
-      <div>
+      <div className="formInput" >
         <label htmlFor="username">
           Username:
           <input
@@ -39,7 +49,7 @@ function RegisterForm() {
           />
         </label>
       </div>
-      <div>
+      <div className="formInput" >
         <label htmlFor="password">
           Password:
           <input
@@ -48,6 +58,53 @@ function RegisterForm() {
             value={password}
             required
             onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+      </div>
+      <div className="formInput" >
+        <label htmlFor="email">
+          Email:
+          <input
+            type="text"
+            name="email"
+            value={email}
+            required
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+      </div>
+      <div className="formInput" >
+        <label htmlFor="first_name">
+          First name:
+          <input
+            type="text"
+            name="first_name"
+            value={first_name}
+            required
+            onChange={(event) => setFirstName(event.target.value)}
+          />
+        </label>
+      </div>
+      <div className="formInput" >
+        <label htmlFor="last_name">
+          Last name:
+          <input
+            type="text"
+            name="last_name"
+            value={last_name}
+            required
+            onChange={(event) => setLastName(event.target.value)}
+          />
+        </label>
+      </div>
+      <div className="formInput" >
+        <label htmlFor="image_url">
+          Image URL:
+          <input
+            type="text"
+            name="image_url"
+            value={image_url}
+            onChange={(event) => setImageUrl(event.target.value)}
           />
         </label>
       </div>
